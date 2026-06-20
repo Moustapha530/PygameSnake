01ffde394e533853880345238c90b0d9ed76d6fe
Moustapha <moustaphamukwini@gmail.com>
2026-06-20 14:37:33 +0200

    create the window and handle events

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Snake Game")
+screen.fill((255, 255, 150))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
